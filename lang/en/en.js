942ee3894b692f75ec1170acfba2706530044835
MarcusLages <marcusvlages@gmail.com>
2025-11-02 22:25:21 -0800

    Changed backend endpoint to new railway one:

--- lang/en/en.js
+++ lang/en/en.js
@@ -5,11 +5,11 @@ export class UserInterfaceString {
 
     static SIGN_UP_BUTTON = "Sign Up"
 
-    static SESSION_DOMAIN = "https://four537-backend-api.onrender.com/api/auth/authenticate"
+    static SESSION_DOMAIN = "https://4537-backend-api-production-dd85.up.railway.app/api/auth/authenticate"
 
-    static LOGIN_DOMAIN = "https://four537-backend-api.onrender.com/api/auth/login"
+    static LOGIN_DOMAIN = "https://4537-backend-api-production-dd85.up.railway.app/api/auth/login"
 
-    static REGISTRATION_DOMAIN = "https://four537-backend-api.onrender.com/api/auth/signup"
+    static REGISTRATION_DOMAIN = "https://4537-backend-api-production-dd85.up.railway.app/api/auth/signup"
 
     static EMAIL_PLACEHOLDER = "Email"
 
