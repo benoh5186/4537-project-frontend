9db73033e757df7da086088c6789dabd14a416cc
Ben Oh <moh16@my.bcit.ca>
2025-11-23 12:37:48 -0800

    add api versio on admin requests

--- lang/en/en.js
+++ lang/en/en.js
@@ -17,9 +17,9 @@ export class UserInterfaceString {
 
     static AI_SERVICE_DOMAIN_TEXT = "https://4537-backend-api-production-dd85.up.railway.app/api/v1/service/ai/text"
 
-    static USERS_DOMAIN = "https://4537-backend-api-production-dd85.up.railway.app/api/admin/users"
+    static USERS_DOMAIN = "https://4537-backend-api-production-dd85.up.railway.app/api/v1/admin/users"
 
-    static DELETE_USER_DOMAIN = "https://4537-backend-api-production-dd85.up.railway.app/api/admin/user/{uid}"
+    static DELETE_USER_DOMAIN = "https://4537-backend-api-production-dd85.up.railway.app/api/v1/admin/user/{uid}"
 
     static EMAIL_PLACEHOLDER = "Email"
 
